4780c042da1401e32bc7aa38b38ed0944e50cef9
suhyun5252 <o52o.suhyun@gmail.com>
2025-02-26 09:55:51 +0900

    webview

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
 import WebView from 'react-native-webview';
 
 const App = (): JSX.Element => {
   const webViewUrl = 'https://app-fish-y3pa.vercel.app';
-  // const webViewUrl = 'http://112.222.157.157:5234/';
+  // SafeAreaView 는 기기의 indicator 영역을 제외한 컨텐츠 영역 배치
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{uri: webViewUrl}} style={styles.webView} />
+      <WebView
+        source={{uri: webViewUrl}} // 웹뷰에 보여줄 URL 주소
+        startInLoadingState={true} // 웹뷰가 로딩 인디케이터 표시
+        renderLoading={() => (
+          // 웹뷰 로딩 중일 때 표시될 로딩 인디케이터 컴포넌트
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#0000ff" />
+            {/* 로딩 스피너 컴포넌트 */}
+          </View>
+        )}
+        style={styles.webview}
+      />
     </SafeAreaView>
   );
 };
@@ -15,11 +26,20 @@ const App = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#111',
+    backgroundColor: '#000',
   },
-  webView: {
+  webview: {
     flex: 1,
   },
+  loadingContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
 });
-
 export default App;
